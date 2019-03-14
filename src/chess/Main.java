7dc3e74827f33df9f14722bd19dff3638ccc794d
Forestf <michalsliwa96@wp.pl>
2019-03-14 19:23:54 +0100

    Create project

---
+++ src/chess/Main.java
@@ -0,0 +1,16 @@
+package chess;
+
+import java.awt.EventQueue;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				new Menu();
+			}
+		});
+		} 
+}
---
+++ src/chess/Menu.java
@@ -0,0 +1,20 @@
+package chess;
+
+import javax.swing.JFrame;
+
+
+public class Menu extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3202086914761975956L;
+
+	public Menu() {
+		super("Chess");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		 this.setVisible(true);
+
+
+	}
+}
