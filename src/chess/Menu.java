0094c06a3c0887f921663505f51d6d2a5131189e
Forestf <michalsliwa96@wp.pl>
2019-03-14 19:42:21 +0100

    Add buttons

--- src/chess/Menu.java
+++ src/chess/Menu.java
@@ -1,19 +1,40 @@
 package chess;
 
+import java.awt.Dimension;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 
 public class Menu extends JFrame {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -3202086914761975956L;
-
+	JPanel menuPanel ;
+	JButton hot;
+	JButton lan;
+	JButton ai;
 	public Menu() {
 		super("Chess");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		 this.setVisible(true);
+		
+		menuPanel = new JPanel();
+		menuPanel.setLayout(new BoxLayout(menuPanel , BoxLayout.PAGE_AXIS));
+		this.setSize(new Dimension(250, 80));
+		
+		this.add(menuPanel);
+		
+		hot = new JButton("Hot-Seat");
+		lan = new JButton("LAN");
+		ai = new JButton("AI");
+		
+		menuPanel.add(hot);
+		menuPanel.add(lan);
+		menuPanel.add(ai);
+		
+		
+		
+		this.setVisible(true);
 
 
 	}
