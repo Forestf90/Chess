be14ee5c3d6397372a927bd17889f259cba561ad
Forestf <michalsliwa96@wp.pl>
2019-03-20 16:57:34 +0100

    Code fix

--- src/chess/GamePanel.java
+++ src/chess/GamePanel.java
@@ -78,72 +78,46 @@ public class GamePanel extends JPanel{
 	private void generatePieces() {
 		
 		for(int i=0 ; i<8 ; i++) {
-			Pawn tempWhite = new Pawn(SideColor.WHITE ,i ,6);
-			Pawn tempBlack = new Pawn(SideColor.BLACK ,i ,1);
-			
-			piecesBoard[i][6] =tempWhite;
-			piecesBoard[i][1] =tempBlack;
+			piecesBoard[i][6] =new Pawn(SideColor.WHITE ,i ,6);;
+			piecesBoard[i][1] =new Pawn(SideColor.BLACK ,i ,1);
 		}
+		
 	}
 	
 	private void generateRooks() {
-		
-		Rook tempWhite1 = new Rook(SideColor.WHITE, 0, 7);
-		Rook tempWhite2 = new Rook(SideColor.WHITE, 7, 7);
-		Rook tempBlack1 = new Rook(SideColor.BLACK, 0, 0);
-		Rook tempBlack2 = new Rook(SideColor.BLACK, 7, 0);
-			
-		piecesBoard[0][7] = tempWhite1;
-		piecesBoard[7][7] = tempWhite2;
-		piecesBoard[0][0] = tempBlack1;
-		piecesBoard[7][0] = tempBlack2;		
+		piecesBoard[0][7] = new Rook(SideColor.WHITE, 0, 7);
+		piecesBoard[7][7] = new Rook(SideColor.WHITE, 7, 7);
+		piecesBoard[0][0] = new Rook(SideColor.BLACK, 0, 0);
+		piecesBoard[7][0] = new Rook(SideColor.BLACK, 7, 0);
 	}
 	
 	private void generateKnights() {
-		
-		Knight tempWhite1 = new Knight(SideColor.WHITE, 1, 7);
-		Knight tempWhite2 = new Knight(SideColor.WHITE, 6, 7);
-		Knight tempBlack1 = new Knight(SideColor.BLACK, 1, 0);
-		Knight tempBlack2 = new Knight(SideColor.BLACK, 6, 0);
-		
-		piecesBoard[1][7] = tempWhite1;
-		piecesBoard[6][7] = tempWhite2;
-		piecesBoard[1][0] = tempBlack1;
-		piecesBoard[6][0] = tempBlack2;		
+		piecesBoard[1][7] = new Knight(SideColor.WHITE, 1, 7);
+		piecesBoard[6][7] = new Knight(SideColor.WHITE, 6, 7);
+		piecesBoard[1][0] = new Knight(SideColor.BLACK, 1, 0);
+		piecesBoard[6][0] = new Knight(SideColor.BLACK, 6, 0);
 	}
 	
 	private void generateBishops() {
-			
-		Bishop tempWhite1 = new Bishop(SideColor.WHITE, 2, 7);
-		Bishop tempWhite2 = new Bishop(SideColor.WHITE, 5, 7);
-		Bishop tempBlack1 = new Bishop(SideColor.BLACK, 2, 0);
-		Bishop tempBlack2 = new Bishop(SideColor.BLACK, 5, 0);
-			
-		piecesBoard[2][7] = tempWhite1;
-		piecesBoard[5][7] = tempWhite2;
-		piecesBoard[2][0] = tempBlack1;
-		piecesBoard[5][0] = tempBlack2;		
+		piecesBoard[2][7] = new Bishop(SideColor.WHITE, 2, 7);
+		piecesBoard[5][7] = new Bishop(SideColor.WHITE, 5, 7);
+		piecesBoard[2][0] = new Bishop(SideColor.BLACK, 2, 0);
+		piecesBoard[5][0] = new Bishop(SideColor.BLACK, 5, 0);	
 	}
 	
 	private void generateQueen() {
-		
-		Queen tempWhite = new Queen(SideColor.WHITE, 3, 7);
-		Queen tempBlack = new Queen(SideColor.BLACK, 3, 0);
-		
-		piecesBoard[3][7] = tempWhite;
-		piecesBoard[3][0] = tempBlack;
+		piecesBoard[3][7] = new Queen(SideColor.WHITE, 3, 7);
+		piecesBoard[3][0] = new Queen(SideColor.BLACK, 3, 0);
 				
 	}
 	
 private void generateKing() {
-		
-		King tempWhite = new King(SideColor.WHITE, 4, 7);
-		King tempBlack = new King(SideColor.BLACK, 4, 0);
-		
-		piecesBoard[4][7] = tempWhite;
-		piecesBoard[4][0] = tempBlack;
+		piecesBoard[4][7] = new King(SideColor.WHITE, 4, 7);
+		piecesBoard[4][0] = new King(SideColor.BLACK, 4, 0);
 				
 	}
+
+
 public void MouseListner() {
 	 addMouseListener(new MouseAdapter(){ 
          public void mousePressed(MouseEvent me) { 
