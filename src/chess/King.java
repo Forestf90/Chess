94beefe0f588aeed7cc4729e69464cf5c950d368
KrzysztofHeller <krzysiek.kez@gmail.com>
2019-03-24 15:57:17 +0100

    Add King Movement
    Add King Movement

--- src/chess/King.java
+++ src/chess/King.java
@@ -15,9 +15,55 @@ public class King extends Chessman {
 		
 		ArrayList<Position> moves = new ArrayList<Position>(); ;
 		
-		if(board[pos.x+1][pos.y+2] == null)
-			moves.add(new Position(this.pos.x+1 ,this.pos.y+2));
-								
+		
+		if(pos.x <= 7 && pos.y+1 <= 7 ) {
+			
+			if(board[pos.x][pos.y+1] == null)
+				moves.add(new Position(this.pos.x ,this.pos.y+1));
+		}
+
+		if(pos.x <= 7 && pos.y-1 >= 0 ) {
+			
+			if(board[pos.x][pos.y-1] == null)
+				moves.add(new Position(this.pos.x ,this.pos.y-1));
+		}
+		
+		if(pos.x+1 <= 7 && pos.y+1 <= 7 ) {
+			
+			if(board[pos.x+1][pos.y+1] == null)
+				moves.add(new Position(this.pos.x+1 ,this.pos.y+1));
+		}
+
+		if(pos.x+1 <= 7 && pos.y-1 >= 0 ) {
+			
+			if(board[pos.x+1][pos.y-1] == null)
+				moves.add(new Position(this.pos.x+1 ,this.pos.y-1));
+		}
+		
+		if(pos.x-1 >= 0 && pos.y+1 <= 7 ) {
+			
+			if(board[pos.x-1][pos.y+1] == null)
+				moves.add(new Position(this.pos.x-1 ,this.pos.y+1));
+		}
+		
+		if(pos.x-1 >= 0 && pos.y-1 >= 0 ) {
+			
+			if(board[pos.x-1][pos.y-1] == null)
+				moves.add(new Position(this.pos.x-1 ,this.pos.y-1));
+		}
+		
+		if(pos.x+1 <= 7 && pos.y <= 7 ) {
+			
+			if(board[pos.x+1][pos.y] == null)
+				moves.add(new Position(this.pos.x+1 ,this.pos.y));
+		}
+		
+		if(pos.x-1 <= 7 && pos.y <= 7 ) {
+			
+			if(board[pos.x-1][pos.y] == null)
+				moves.add(new Position(this.pos.x-1 ,this.pos.y));
+		}
+		
 		return moves;
 		
 	}
