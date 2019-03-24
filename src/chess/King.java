076c03e63f3a4b2407a573dbd4174073e7c1b784
KrzysztofHeller <krzysiek.kez@gmail.com>
2019-03-24 18:03:17 +0100

    New King Movements
    New King Movements

--- src/chess/King.java
+++ src/chess/King.java
@@ -4,67 +4,115 @@ import java.util.ArrayList;
 
 public class King extends Chessman {
 
-	public King(SideColor col , int x , int y) {
-		color =col;
-		pos= new Position(x ,y);
-		
+	public King(SideColor col, int x, int y) {
+		color = col;
+		pos = new Position(x, y);
+
 		loadImage();
-		
+
 	}
+
 	public ArrayList<Position> GetMoves(Chessman[][] board) {
-		
-		ArrayList<Position> moves = new ArrayList<Position>(); ;
-		
-		
-		if(pos.x <= 7 && pos.y+1 <= 7 ) {
+
+		ArrayList<Position> moves = new ArrayList<Position>();
+		;
+
+		if (pos.x <= 7 && pos.y + 1 <= 7) {
+
+			if (board[pos.x][pos.y + 1] == null)
+				moves.add(new Position(this.pos.x, this.pos.y + 1));
+
 			
-			if(board[pos.x][pos.y+1] == null)
-				moves.add(new Position(this.pos.x ,this.pos.y+1));
+			  if(board[this.pos.x][this.pos.y+1] !=null) {
+			  if(board[this.pos.x][this.pos.y+1].color != this.color) moves.add(new
+			  Position(this.pos.x ,this.pos.y+1)); }
+			 
+		}
+		if (pos.x <= 7 && pos.y - 1 >= 0) {
+
+			if (board[pos.x][pos.y - 1] == null)
+				moves.add(new Position(this.pos.x, this.pos.y - 1));
+
+			
+			  if(board[this.pos.x][this.pos.y-1] !=null) {
+			  if(board[this.pos.x][this.pos.y-1].color != this.color) moves.add(new
+			  Position(this.pos.x ,this.pos.y-1)); }
+			 
 		}
 
-		if(pos.x <= 7 && pos.y-1 >= 0 ) {
+		if (pos.x + 1 <= 7 && pos.y + 1 <= 7) {
+
+			if (board[pos.x + 1][pos.y + 1] == null)
+				moves.add(new Position(this.pos.x + 1, this.pos.y + 1));
+
 			
-			if(board[pos.x][pos.y-1] == null)
-				moves.add(new Position(this.pos.x ,this.pos.y-1));
-		}
-		
-		if(pos.x+1 <= 7 && pos.y+1 <= 7 ) {
-			
-			if(board[pos.x+1][pos.y+1] == null)
-				moves.add(new Position(this.pos.x+1 ,this.pos.y+1));
+			  if(board[this.pos.x+1][this.pos.y+1] !=null) {
+			  if(board[this.pos.x+1][this.pos.y+1].color != this.color) moves.add(new
+			  Position(this.pos.x+1 ,this.pos.y+1)); }
+			 
 		}
 
-		if(pos.x+1 <= 7 && pos.y-1 >= 0 ) {
+		if (pos.x + 1 <= 7 && pos.y - 1 >= 0) {
+
+			if (board[pos.x + 1][pos.y - 1] == null)
+				moves.add(new Position(this.pos.x + 1, this.pos.y - 1));
+
 			
-			if(board[pos.x+1][pos.y-1] == null)
-				moves.add(new Position(this.pos.x+1 ,this.pos.y-1));
+			  if(board[this.pos.x+1][this.pos.y-1] !=null) {
+			  if(board[this.pos.x+1][this.pos.y-1].color != this.color) moves.add(new
+			  Position(this.pos.x+1 ,this.pos.y-1)); }
+			 
 		}
-		
-		if(pos.x-1 >= 0 && pos.y+1 <= 7 ) {
+
+		if (pos.x - 1 >= 0 && pos.y + 1 <= 7) {
+
+			if (board[pos.x - 1][pos.y + 1] == null)
+				moves.add(new Position(this.pos.x - 1, this.pos.y + 1));
+
 			
-			if(board[pos.x-1][pos.y+1] == null)
-				moves.add(new Position(this.pos.x-1 ,this.pos.y+1));
+			  if(board[this.pos.x-1][this.pos.y+1] !=null) {
+			  if(board[this.pos.x-1][this.pos.y+1].color != this.color) moves.add(new
+			  Position(this.pos.x-1 ,this.pos.y+1)); }
+			 
 		}
-		
-		if(pos.x-1 >= 0 && pos.y-1 >= 0 ) {
+
+		if (pos.x - 1 >= 0 && pos.y - 1 >= 0) {
+
+			if (board[pos.x - 1][pos.y - 1] == null)
+				moves.add(new Position(this.pos.x - 1, this.pos.y - 1));
+
 			
-			if(board[pos.x-1][pos.y-1] == null)
-				moves.add(new Position(this.pos.x-1 ,this.pos.y-1));
+			  if(board[this.pos.x-1][this.pos.y-1] !=null) {
+			  if(board[this.pos.x-1][this.pos.y-1].color != this.color) moves.add(new
+			  Position(this.pos.x-1 ,this.pos.y-1)); }
+			 
 		}
-		
-		if(pos.x+1 <= 7 && pos.y <= 7 ) {
+
+		if (pos.x + 1 <= 7 && pos.y <= 7) {
+
+			if (board[pos.x + 1][pos.y] == null)
+				moves.add(new Position(this.pos.x + 1, this.pos.y));
+
 			
-			if(board[pos.x+1][pos.y] == null)
-				moves.add(new Position(this.pos.x+1 ,this.pos.y));
+			  if(board[this.pos.x+1][this.pos.y] !=null) {
+			  if(board[this.pos.x+1][this.pos.y].color != this.color) moves.add(new
+			  Position(this.pos.x+1 ,this.pos.y)); }
+			 
 		}
-		
-		if(pos.x-1 <= 7 && pos.y <= 7 ) {
+
+		if (pos.x - 1 >= 0 && pos.y <= 7) {
+
+			if (board[pos.x - 1][pos.y] == null)
+				moves.add(new Position(this.pos.x - 1, this.pos.y));
+
 			
-			if(board[pos.x-1][pos.y] == null)
-				moves.add(new Position(this.pos.x-1 ,this.pos.y));
+			  if(board[this.pos.x-1][this.pos.y] !=null) {
+			  if(board[this.pos.x-1][this.pos.y].color != this.color) moves.add(new
+			  Position(this.pos.x-1 ,this.pos.y)); }
+			 
 		}
-		
+
 		return moves;
-		
+
 	}
 }
