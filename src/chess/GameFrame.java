66bca9a2c7fbc6d71783c7af422aa07757f90ea1
Forestf <michalsliwa96@wp.pl>
2019-04-13 14:10:32 +0200

    Disable resize

--- src/chess/GameFrame.java
+++ src/chess/GameFrame.java
@@ -5,6 +5,7 @@ import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
 
 import javax.swing.JFrame;
+import javax.swing.JMenuBar;
 
 import chess.panels.GamePanel;
 
@@ -17,13 +18,20 @@ public class GameFrame extends JFrame implements ComponentListener {
 	public GameFrame(GamePanel gamePanel){
 		super("Chess");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		panel = gamePanel;
 		this.add(panel);
 		addComponentListener(this);
+//		this.getContentPane().setPreferredSize(new Dimension(gamePanel.SQUARE_SIZE *8,gamePanel.SQUARE_SIZE*8 ));
+//		this.getContentPane().setBounds(0, 0, gamePanel.SQUARE_SIZE *8, gamePanel.SQUARE_SIZE *8);
+		this.setJMenuBar(new JMenuBar());
+		//this.getJMenuBar().setBounds(0, 0, getWidth(), 25);
+		this.setResizable(false);
 		this.pack();
 		this.setLocationRelativeTo(null);
 		this.setVisible(true);
+		
+		
 		height= 64*8;
 		width = 64*8;
 	}
